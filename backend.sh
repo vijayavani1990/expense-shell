74b65988a83637f250decead6764afb7a71aa35e
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-11-18 23:03:16 +0530

    expense project configuration

---
+++ backend.sh
@@ -0,0 +1,23 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+
+dnf install nodejs -y
+
+cp backend.service /etc/system.d/system/backend.service
+
+useradd expense
+
+mkdir /app
+
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+
+npm install
+
+dnf install mysql -y
+mysql -h mysql-dev.vijayavanimanju.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+
+systemctl daemon-reload
+systemctl enable backend
+systemctl start backend
