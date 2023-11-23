f2970a4a8dd6913c04930cd679056077f8a2a3fe
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-11-23 22:28:17 +0530

    expense project configuration

--- backend.sh
+++ backend.sh
@@ -1,23 +1,48 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+color="\e[36m"
 
-dnf install nodejs -y
+echo -e "${color} Disable nodejs default version \e[0m"
+dnf module disable nodejs -y &>>/tmp/backend.log
+echo $?
+echo -e "${color} Enable nodejs 18 version \e[0m"
+dnf module enable nodejs:18 -y &>>/tmp/backend.log
+echo $?
 
-cp backend.service /etc/systemd/system/backend.service
+echo -e "${color} Install nodejs 18 version \e[0m"
+dnf install nodejs -y &>>/tmp/backend.log
+echo $?
 
-useradd expense
+echo -e "${color} Create backend service file \e[0m"
+cp backend.service /etc/systemd/system/backend.service &>>/tmp/backend.log
+echo $?
 
-mkdir /app
+echo -e "${color} Create user named expense for Application\e[0m"
+useradd expense &>>/tmp/backend.log
+echo $?
 
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-cd /app
-unzip /tmp/backend.zip
+echo -e "${color} Create directory called app for Application \e[0m"
+mkdir /app &>>/tmp/backend.log
+echo $?
 
-npm install
+echo -e "${color} Download backend application content \e[0m"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>/tmp/backend.log
+cd /app &>>/tmp/backend.log
+echo -e "${color} Extract backend application content \e[0m"
+unzip /tmp/backend.zip &>>/tmp/backend.log
+echo $?
 
-dnf install mysql -y
-mysql -h mysql-dev.vijayavanimanju.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo -e "${color} Download nodejs dependencies \e[0m"
+npm install &>>/tmp/backend.log
+echo $?
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
+echo -e "${color} Install mysql client to load the Schema\e[0m"
+dnf install mysql -y &>>/tmp/backend.log
+echo $?
+echo -e "${color} Load the schema \e[0m"
+mysql -h mysql-dev.vijayavanimanju.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/backend.log
+echo $?
+
+echo -e "${color} Start backend service \e[0m"
+systemctl daemon-reload &>>/tmp/backend.log
+systemctl enable backend &>>/tmp/backend.log
+systemctl start backend &>>/tmp/backend.log
+echo $?
--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,20 @@
+echo -e "\e[35m Disable existing mysql module \e[0m"
 dnf module disable mysql -y
+echo $?
 
+echo -e "\e[35m Create mysql repo file \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo $?
 
+echo -e "\e[35m install mysql community server \e[0m"
 dnf install mysql-community-server -y
+echo $?
 
+echo -e "\e[35m enable and restart mysql server \e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+echo $?
 
+echo -e "\e[35m set the password for sql database \e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1
+echo $?
