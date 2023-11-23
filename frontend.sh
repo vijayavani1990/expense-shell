bcf440c11dbe3977cbc63f13a5cd893dc9deca13
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-11-23 22:03:47 +0530

    expense project configuration

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,22 @@
-echo -e "\e[34m Installing Nginx \e[0m"
-dnf install nginx -y &>>/tmp/expense.log
+log_file=/tmp/expense.log
+blue=\e[34m
 
-echo -e "\e[34m Copy Expense Config file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+echo -e "${blue} Installing Nginx \e[0m"
+dnf install nginx -y &>>$log_file
 
-echo -e "\e[34m Clean Old Nginx file \e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
+echo -e "${blue} Copy Expense Config file \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
-echo -e "\e[34m Download Frontend application code \e[0m"
+echo -e "${blue} Clean Old Nginx file \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+
+echo -e "${blue} Download Frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
-echo -e "\e[34m Extract the downloaded frontend code \e[0m"
-cd /usr/share/nginx/html &>>/tmp/expense.log
-unzip /tmp/frontend.zip &>>/tmp/expense.log
+echo -e "${blue} Extract the downloaded frontend code \e[0m"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
 
-echo -e "\e[34m Restart Nginx service \e[0m"
-systemctl enable nginx &>>/tmp/expense.log
-systemctl restart nginx &>>/tmp/expense.log
+echo -e "${blue} Restart Nginx service \e[0m"
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
