7cfead5c7d34d0de6b19d70a472d58017567f421
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-11-23 22:11:50 +0530

    expense project configuration

--- frontend.sh
+++ frontend.sh
@@ -3,20 +3,28 @@ blue="\e[34m"
 
 echo -e "${blue} Installing Nginx \e[0m"
 dnf install nginx -y &>>$log_file
+echo $?
 
 echo -e "${blue} Copy Expense Config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo $?
 
 echo -e "${blue} Clean Old Nginx file \e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
+echo $?
 
 echo -e "${blue} Download Frontend application code \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo $?
 
 echo -e "${blue} Extract the downloaded frontend code \e[0m"
 cd /usr/share/nginx/html &>>$log_file
+echo $?
 unzip /tmp/frontend.zip &>>$log_file
+echo $?
 
 echo -e "${blue} Restart Nginx service \e[0m"
 systemctl enable nginx &>>$log_file
+echo $?
 systemctl restart nginx &>>$log_file
+echo $?
