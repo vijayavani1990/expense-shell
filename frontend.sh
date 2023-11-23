dc607fde3aae4465ce4f0b356a2da1678faa238d
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-11-23 21:35:36 +0530

    expense project configuration

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
 echo -e "\e[34m Installing Nginx \e[0m"
-dnf install nginx -y
+dnf install nginx -y &>>/tmp/expense.log
 
 echo -e "\e[34m Copy Expense Config file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 
 echo -e "\e[34m Clean Old Nginx file \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 
 echo -e "\e[34m Download Frontend application code \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
 echo -e "\e[34m Extract the downloaded frontend code \e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>/tmp/expense.log
+unzip /tmp/frontend.zip &>>/tmp/expense.log
 
 echo -e "\e[34m Restart Nginx service \e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>/tmp/expense.log
+systemctl restart nginx &>>/tmp/expense.log
